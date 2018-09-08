002f0e9301ce654b11d6610cf4b8dbb5ab474306
forestriveral <forestriveral@gmail.com>
2018-09-08 20:39:00 +0800

    Training stage 1

--- concrete_mrcnn/concrete.py
+++ concrete_mrcnn/concrete.py
@@ -11,12 +11,12 @@ import sys
 import numpy as np
 import cv2
 import imgaug
-# import skimage.io
-# import matplotlib
+import skimage.io
+import matplotlib
 import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../")
+ROOT_DIR = os.path.abspath(".")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -35,6 +35,7 @@ from pycocotools import mask as maskUtils
 # import coco
 
 # %matplotlib inline
+DATASETS_PATH = "datasets"
 
 # Directory to save logs and trained model
 DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
@@ -45,6 +46,10 @@ COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mrcnn/mask_rcnn_coco.h5")
 # Results directory
 # Save submission files here
 RESULTS_DIR = os.path.join(ROOT_DIR, "results/concrete/")
+
+# Directory of images to run detection on
+# IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+IMAGE_DIR = os.path.join(ROOT_DIR, DATASETS_PATH, "images")
 
 # Download COCO trained weights from Releases if needed
 # if not os.path.exists(COCO_MODEL_PATH):
@@ -83,13 +88,28 @@ class ConcreteConfig(Config):
     TRAIN_ROIS_PER_IMAGE = 32
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 25
 
     # use small validation steps since the epoch is small
     VALIDATION_STEPS = 5
 
 # config = ConcreteConfig()
 # config.display()
+
+class InferenceConfig(ConcreteConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+    DETECTION_MIN_CONFIDENCE = 0.7
+
+    # Don't resize imager for inferencing
+    IMAGE_RESIZE_MODE = "pad64"
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.7
+
 
 def get_ax(rows=1, cols=1, size=8):
     """Return a Matplotlib Axes array to be used in
@@ -120,32 +140,43 @@ class ConcreteDataset(utils.Dataset):
         # Add classes
         # self.add_class("concrete", 1, "bughole")
         # self.add_class("concrete", 2, "crack")
-        # self.add_class("shapes", 3, "triangle")
 
         # Train or validation dataset?
-        assert subset in ["train", "val"]
-        coco = COCO("{}/annotations/{}.json".format(dataset_dir, subset))
-        image_dir = "{}/{}".format(dataset_dir, subset)
+        assert subset in ["train", "val", "test"]
+        if subset == "train" or "val":
+            coco = COCO("{}/annotations/{}.json".format(dataset_dir, subset))
+            image_dir = "{}/{}".format(dataset_dir, subset)
 
-        # Get class and images ids
-        class_ids = sorted(coco.getCatIds())
-        image_ids = list(coco.imgs.keys())
+            # Get class and images ids
+            class_ids = sorted(coco.getCatIds())
+            image_ids = list(coco.imgs.keys())
 
-        # Add classes
-        for i in class_ids:
-            self.add_class("concrete", i, coco.loadCats(i)[0]["name"])
+            # Add classes
+            for i in class_ids:
+                self.add_class("concrete", i, coco.loadCats(i)[0]["name"])
 
-        # Add images
-        for i in image_ids:
-            self.add_image(
-                "concrete", image_id=i,
-                path=os.path.join(image_dir, coco.imgs[i]['file_name']),
-                width=coco.imgs[i]["width"],
-                height=coco.imgs[i]["height"],
-                annotations=coco.loadAnns(coco.getAnnIds(
-                    imgIds=[i], catIds=class_ids, iscrowd=None)))
-        if return_coco:
-            return coco
+            # Add images
+            for i in image_ids:
+                self.add_image(
+                    "concrete", image_id=i,
+                    path=os.path.join(image_dir, coco.imgs[i]['file_name']),
+                    width=coco.imgs[i]["width"],
+                    height=coco.imgs[i]["height"],
+                    annotations=coco.loadAnns(coco.getAnnIds(
+                        imgIds=[i], catIds=class_ids, iscrowd=None)))
+            if return_coco:
+                return coco
+        else:
+            dataset_path = os.path.join(dataset_dir, subset)
+            # Get image ids from directory names
+            image_ids = next(os.walk(dataset_path))[2]
+
+            # Add images
+            for image_id in image_ids:
+                self.add_image(
+                    "concrete",
+                    image_id=image_id,
+                    path=os.path.join(dataset_path, "{}.jpg".format(image_id)))
 
     def image_reference(self, image_id):
         """Return the shapes data of the image."""
@@ -241,6 +272,71 @@ class ConcreteDataset(utils.Dataset):
         return m
 
 ############################################################
+#  Training
+############################################################
+
+def train(model, dataset_dir, subset, train_mode):
+    """Train the model."""
+    # Training dataset. Use the training set and 35K from the
+    # validation set, as as in the Mask RCNN paper.
+    dataset_train = ConcreteDataset()
+    dataset_train.load_concrete(dataset_dir, subset)
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = ConcreteDataset()
+    dataset_val.load_concrete(dataset_dir, "val")
+    dataset_val.prepare()
+
+    # Image Augmentation
+    # Right/Left flip 50% of the time
+    # augmentation = imgaug.augmenters.Fliplr(0.5)
+    augmentation = imgaug.augmenters.SomeOf((0, 2), [
+        imgaug.augmenters.Fliplr(0.5),
+        imgaug.augmenters.Flipud(0.3),
+        imgaug.augmenters.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255),
+                                                per_channel=0.5),
+        imgaug.augmenters.CropAndPad(percent=(-0.05, 0.1), pad_mode=imgaug.ALL, pad_cval=(0, 255)),
+        imgaug.augmenters.GaussianBlur(sigma=(0.0, 3.0)),
+        imgaug.augmenters.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+        imgaug.augmenters.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2)
+        ], random_order=True)
+
+    # *** This training schedule is an example. Update to your needs ***
+
+    if train_mode == "1":
+        # Training - Stage 1
+        print("Training network heads")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=30,
+                    layers='heads',
+                    augmentation=augmentation)
+
+    elif train_mode == "2":
+        # Training - Stage 2
+        # Finetune layers from ResNet stage 4 and up
+        print("Fine tune Resnet stage 4 and up")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE,
+                    epochs=90,
+                    layers='4+',
+                    augmentation=augmentation)
+
+    elif train_mode == "3":
+        # Training - Stage 3
+        # Fine tune all layers
+        print("Fine tune all layers")
+        model.train(dataset_train, dataset_val,
+                    learning_rate=config.LEARNING_RATE / 10,
+                    epochs=120,
+                    layers='all',
+                    augmentation=augmentation)
+    else:
+        print("'{}' is Invalid training mode".format(args.train_mode))
+
+
+############################################################
 #  Evaluation
 ############################################################
 
@@ -323,7 +419,112 @@ def evaluate_concrete(model, dataset, coco, eval_type="bbox", limit=0, image_ids
     print("Total time: ", time.time() - t_start)
 
 ############################################################
-#  Training
+#  RLE Encoding
+############################################################
+
+def rle_encode(mask):
+    """Encodes a mask in Run Length Encoding (RLE).
+    Returns a string of space-separated values.
+    """
+    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
+    # Flatten it column wise
+    m = mask.T.flatten()
+    # Compute gradient. Equals 1 or -1 at transition points
+    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
+    # 1-based indicies of transition points (where gradient != 0)
+    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
+    # Convert second index in each pair to lenth
+    rle[:, 1] = rle[:, 1] - rle[:, 0]
+    return " ".join(map(str, rle.flatten()))
+
+
+def rle_decode(rle, shape):
+    """Decodes an RLE encoded list of space separated
+    numbers and returns a binary mask."""
+    rle = list(map(int, rle.split()))
+    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
+    rle[:, 1] += rle[:, 0]
+    rle -= 1
+    mask = np.zeros([shape[0] * shape[1]], np.bool)
+    for s, e in rle:
+        assert 0 <= s < mask.shape[0]
+        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
+        mask[s:e] = 1
+    # Reshape and transpose
+    mask = mask.reshape([shape[1], shape[0]]).T
+    return mask
+
+
+def mask_to_rle(image_id, mask, scores):
+    "Encodes instance masks to submission format."
+    assert mask.ndim == 3, "Mask must be [H, W, count]"
+    # If mask is empty, return line with image ID only
+    if mask.shape[-1] == 0:
+        return "{},".format(image_id)
+    # Remove mask overlaps
+    # Multiply each instance mask by its score order
+    # then take the maximum across the last dimension
+    order = np.argsort(scores)[::-1] + 1  # 1-based descending
+    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
+    # Loop over instance masks
+    lines = []
+    for o in order:
+        m = np.where(mask == o, 1, 0)
+        # Skip if empty
+        if m.sum() == 0.0:
+            continue
+        rle = rle_encode(m)
+        lines.append("{}, {}".format(image_id, rle))
+    return "\n".join(lines)
+
+############################################################
+#  Detection
+############################################################
+
+def detect(model, dataset_dir, subset):
+    """Run detection on images in the given directory."""
+    print("Running on {}".format(dataset_dir))
+
+    # Create directory
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
+    os.makedirs(submit_dir)
+
+    # Read dataset
+    dataset = ConcreteDataset()
+    dataset.load_concrete(dataset_dir, subset)
+    dataset.prepare()
+    # Load over images
+    submission = []
+    for image_id in dataset.image_ids:
+        # Load image and run detection
+        image = dataset.load_image(image_id)
+        # Detect objects
+        r = model.detect([image], verbose=1)[0]
+        # Encode image to RLE. Returns a string of multiple lines
+        source_id = dataset.image_info[image_id]["id"]
+        rle = mask_to_rle(source_id, r["masks"], r["scores"])
+        submission.append(rle)
+        # Save image with masks
+        visualize.display_instances(
+            image, r['rois'], r['masks'], r['class_ids'],
+            dataset.class_names, r['scores'],
+            show_bbox=True, show_mask=True,
+            title="Predictions")
+        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+
+    # Save to csv file
+    submission = "ImageId, EncodedPixels\n" + "\n".join(submission)
+    file_path = os.path.join(submit_dir, "submit.csv")
+    with open(file_path, "w") as f:
+        f.write(submission)
+    print("Saved to ", submit_dir)
+
+
+############################################################
+#  Run Training
 ############################################################
 
 if __name__ == '__main__':
@@ -334,16 +535,17 @@ if __name__ == '__main__':
         description='Train Mask R-CNN on Concrete COCO-datasets.')
     parser.add_argument("command",
                         metavar="<command>",
-                        help="'train' or 'evaluate' on Concrete COCO-datasets")
-    parser.add_argument('--model', required=True,
+                        help="'train' or 'evaluate' or 'detect' on Concrete COCO-datasets")
+    parser.add_argument('--weights', required=False,
+                        default="coco",
                         metavar="/path/to/weights.h5",
                         help="Path to weights .h5 file or 'coco'")
     parser.add_argument('--dataset', required=False,
-                        default="../datasets",
+                        default=DATASETS_PATH,
                         metavar="/path/to/coco/",
                         help='Directory of the Concrete COCO-datasets dataset')
-    parser.add_argument('--train mode', required=False,
-                        default= 1,
+    parser.add_argument('--train_mode', required=False,
+                        default= "1",
                         metavar="Training - Stage 1 ,2 or 3",
                         help="Choose to train in different stages: \n" +
                              "Stage 1 ==> Training network heads (default) \n" +
@@ -357,24 +559,41 @@ if __name__ == '__main__':
                         default= 20,
                         metavar="<image count>",
                         help='Images to use for evaluation (default=20)')
-
+    parser.add_argument('--subset', required=False,
+                        metavar="Specific dataset sub-directory to detection",
+                        help="Subset of dataset to run test")
+    parser.add_argument('--filename', required=False,
+                        metavar="Choose specific images you want to be detected ",
+                        help="Image filename provided when detect one images")
     args = parser.parse_args()
+
+    if args.command == "detect":
+        assert args.subset, "Provide --subset or one image to run prediction on"
+        if args.subset == "images":
+            assert args.filename, "Provide --which image do you want to detect?"
+
     print("Command: ", args.command)
-    print("Model: ", args.model)
+    print("Weights: ", args.weights)
     print("Dataset: ", args.dataset)
+    if args.subset:
+        print("Subset: ", args.subset)
     print("Logs: ", args.logs)
-    print("Train mode: ", args.train_mode)
+    if args.command == "train":
+        assert args.train_mode in ["1", "2", "3"]
+        if args.train_mode =="1":
+            print("Train mode: Network heads")
+        elif args.train_mode =="2":
+            print("Train mode: Fine tune Resnet stage 4 and up")
+        elif args.train_mode =="3":
+            print("Train mode: Fine tune all layers")
+        else:
+            print("Invalid Train mode. Use the default mode")
+            args.train_mode = "1"
 
     # Configurations
     if args.command == "train":
         config = ConcreteConfig()
     else:
-        class InferenceConfig(ConcreteConfig):
-            # Set batch size to 1 since we'll be running inference on
-            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-            GPU_COUNT = 1
-            IMAGES_PER_GPU = 1
-            DETECTION_MIN_CONFIDENCE = 0
         config = InferenceConfig()
     config.display()
 
@@ -387,86 +606,56 @@ if __name__ == '__main__':
                                   model_dir=args.logs)
 
     # Select weights file to load
-    if args.model.lower() == "coco":
-        model_path = COCO_WEIGHTS_PATH
-    elif args.model.lower() == "last":
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+    elif args.weights.lower() == "last":
         # Find last trained weights
-        model_path = model.find_last()
+        weights_path = model.find_last()
     # elif args.model.lower() == "imagenet":
     #     # Start from ImageNet trained weights
     #     model_path = model.get_imagenet_weights()
     else:
-        model_path = args.model
+        weights_path = args.weights
 
     # Load weights
-    print("Loading weights ", model_path)
-    model.load_weights(model_path, by_name=True)
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
 
-    # Train or evaluate
+    # Train or evaluate or detect
     if args.command == "train":
-        # Training dataset. Use the training set and 35K from the
-        # validation set, as as in the Mask RCNN paper.
-        dataset_train = ConcreteDataset()
-        dataset_train.load_concrete(args.dataset, "train")
-        dataset_train.prepare()
-
-        # Validation dataset
-        dataset_val = ConcreteDataset()
-        val_type = "val"
-        dataset_val.load_concrete(args.dataset, val_type)
-        dataset_val.prepare()
-
-        # Image Augmentation
-        # Right/Left flip 50% of the time
-        augmentation = imgaug.augmenters.Fliplr(0.5)
-
-        # *** This training schedule is an example. Update to your needs ***
-
-        if args.train_mode == "1":
-            # Training - Stage 1
-            print("Training network heads")
-            model.train(dataset_train, dataset_val,
-                        learning_rate=config.LEARNING_RATE,
-                        epochs=40,
-                        layers='heads',
-                        augmentation=augmentation)
-
-        elif args.train_mode == "2":
-            # Training - Stage 2
-            # Finetune layers from ResNet stage 4 and up
-            print("Fine tune Resnet stage 4 and up")
-            model.train(dataset_train, dataset_val,
-                        learning_rate=config.LEARNING_RATE,
-                        epochs=120,
-                        layers='4+',
-                        augmentation=augmentation)
-
-        elif args.train_mode == "3":
-            # Training - Stage 3
-            # Fine tune all layers
-            print("Fine tune all layers")
-            model.train(dataset_train, dataset_val,
-                        learning_rate=config.LEARNING_RATE / 10,
-                        epochs=160,
-                        layers='all',
-                        augmentation=augmentation)
-        else:
-            print("Invalid train mode")
-
+        train(model, args.dataset, "train", args.train_mode)
     elif args.command == "evaluate":
         # Validation dataset
         dataset_val = ConcreteDataset()
         val_type = "val"
         coco = dataset_val.load_concrete(args.dataset, val_type, return_coco=True)
         dataset_val.prepare()
-        print("Running COCO evaluation on {} images.".format(args.limit))
+        print("Running concrete evaluation on {} images.".format(args.limit))
         evaluate_concrete(model, dataset_val, coco, "bbox", limit=int(args.limit))
+    elif args.command == "detect":
+        assert args.subset in ["test","images"]
+        if args.subset == "test":
+            detect(model, args.dataset, "test")
+        else:
+            class_names = ['BG', 'crack', 'bughole']
+            file_names = next(os.walk(IMAGE_DIR))[2]
+            image = skimage.io.imread(os.path.join(IMAGE_DIR, args.filename))
+
+            # Run detection
+            results = model.detect([image], verbose=1)
+
+            # Visualize results
+            r = results[0]
+            visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                                        class_names, r['scores'])
     else:
         print("'{}' is not recognized. "
-              "Use 'train' or 'evaluate'".format(args.command))
-
-
-
-
-
+              "Use 'train', 'evaluate' or 'detect'".format(args.command))
 
